fc060194d883089f7349adae46404366eaf1fbdf
machester <shusheng1991@gmail.com>
2019-04-10 00:52:46 +0800

    update tmp112 driver

--- TEMP/Inc/chip_tmp112.h
+++ TEMP/Inc/chip_tmp112.h
@@ -9,6 +9,7 @@
 #define CHIP_TMP112_H_
 
 #include "main.h"
+#include "i2c.h"
 
 /** 12bit temperatures -----------------------------------------------------*/
 #define temp_128_degree				0x7ff
@@ -23,9 +24,24 @@
 #define temp_neg_25_degreee			0xe70
 #define temp_neg_55_degree			0xc90
 
+typedef enum {
+	mode_12bit = 0, mode_13bit = 1
+} tmp112_data_width;
+
+#define rate_p25Hz  0		// CR1 = 0, CR0 = 0
+#define rate_1Hz 	1		// CR1 = 0, CR0 = 1
+#define rate_4Hz	2		// CR1 = 1, CR0 = 0
+#define rate_8Hz	3		// CR1 = 1, CR0 = 1
+
+#define TMP_CONTROL_REG			0x00
+#define temp_reg_read_only 		(TMP_CONTROL_REG | (0 << 0))			// P1 = 0, P0 = 0
+#define config_reg				(TMP_CONTROL_REG | (1 << 0))			// P1 = 0, P0 = 1
+#define temp_low_reg			(TMP_CONTROL_REG | (2 << 0))			// P1 = 1, P0 = 0
+#define temp_high_reg			(TMP_CONTROL_REG | (3 << 0))			// P1 = 1, P0 = 1
+#define chip_address			0x90
 
 ErrorStatus
-tmp112_init (void);
+tmp112_init(void);
 
 int
 temp_transfer(unsigned int degree);
--- TEMP/Src/chip_tmp112.c
+++ TEMP/Src/chip_tmp112.c
@@ -5,20 +5,20 @@
  *      Author: shush
  */
 
-
 #include "chip_tmp112.h"
 
-ErrorStatus
-tmp112_init (void)
-{
-	ErrorStatus status;
-
-	return status;
+ErrorStatus tmp112_init(void) {
+	HAL_StatusTypeDef status;
+	uint8_t
+	buff[] = { config_reg, rate_1Hz };
+	status = HAL_I2C_Master_Transmit(&hi2c1, chip_address, buff, 2, 30);
+	if (HAL_OK != status)
+		return ERROR;
+	else
+		return SUCCESS;
 }
 
-int
-temp_transfer(unsigned int degree)
-{
+int temp_transfer(unsigned int degree) {
 	uint32_t clear_flag = 0x0fff;
 	degree = degree & clear_flag;
 	return (800 * (0.0625 / degree));
